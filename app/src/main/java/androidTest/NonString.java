0f3489d169124686259a61b3a145ba26711a0b58
rehamsamy <reham.samy2012@yahoo.com>
2019-04-12 01:39:15 -0700

    now

---
+++ app/src/main/java/androidTest/NonString.java
@@ -0,0 +1,32 @@
+package androidTest;
+
+import android.util.Log;
+
+import org.junit.runner.RunWith;
+
+import builditbigger.EndpointsAsyncTask;
+
+
+import static java.security.AccessController.getContext;
+import static junit.framework.TestCase.assertNotNull;
+
+//@RunWith(AndroidJUnit4.class)
+public class NonString {
+//
+//    private static final String LOG_TAG = NonString.class.getSimpleName();
+//
+//
+//    public void test() {
+//        String result = null;
+//       EndpointsAsyncTask task=new EndpointsAsyncTask(getContext());
+//        task.execute();
+//        try {
+//            result = task.get();
+//            Log.d(LOG_TAG, "Retrieved a non-empty string successfully: " + result);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        assertNotNull(result);
+//    }
+
+}
